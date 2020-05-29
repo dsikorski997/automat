8b4533abf93f8fed0318a9277f4ed3a0a366f599
DS <DS@DS>
2020-05-29 20:30:06 +0200

    menu wplaty i wyboru

--- automat/menu.cpp
+++ automat/menu.cpp
@@ -1,1 +1,67 @@
 #include "menu.h"
+
+
+Menu::Menu() //konstruktor wypelnia zmienne przy tworzeniu obiektu
+{
+    flaga_menu_wplaty = 0; //flaga petli wyswietlania
+    flaga_menu_wyboru = 0; //flaga petli wyswietlania
+    flaga_menu_serwisowe = 0; //flaga petli wyswietlania
+    wybor = 7; //zmienna do switcha
+    suma = 0; //zmienna przechowujaca sume wplaty
+    wplata[6]; //tablica przechowujaca dane o ilosci wrzuconych monet
+    for(int i = 0 ; i <= 5 ; i++) wplata[i] = 0;
+    //for(int i = 0 ; i <= 5 ; i++) std::cout << wplata[i] << std::endl;
+
+}
+
+Menu::~Menu()
+{
+
+};
+
+void Menu::wyswietl_menu_wplaty()
+{
+flaga_menu_wplaty = 1;
+    while(flaga_menu_wplaty){
+        std::cout<<"Automat przyjmuje monety 5, 2, 1, 0.50, 0.20, 0.10 zl";
+        std::cout<<"\nWpisz jakie wrzuciles monety\n";
+        std::cout<<"1: 5zl\n";
+        std::cout<<"2: 2zl\n";
+        std::cout<<"3: 1zl\n";
+        std::cout<<"4: 0.50zl\n";
+        std::cout<<"5: 0.20zl\n";
+        std::cout<<"6: 0.10zl\n";
+        std::cout<<"7: Zakoncz wplacanie\n";
+        std::cout<< "suma: " << std::fixed << std::setprecision(2) << suma <<std::endl;
+        std::cin>>wybor;
+
+
+            switch(wybor){
+                case 1: wplata[0]++; break;
+                case 2: wplata[1]++; break;
+                case 3: wplata[2]++; break;
+                case 4: wplata[3]++; break;
+                case 5: wplata[4]++; break;
+                case 6: wplata[5]++; break;
+                case 7: wyswietl_menu_wyboru(); flaga_menu_wplaty = 0; break;
+                case 9: wyswietl_menu_serwisowe(); flaga_menu_wplaty = 0; break; //ukryta opcja wejscia w menu serwisowe
+                case 0: flaga_menu_wplaty = 0; break; //wyjscie z programuplan jest taki
+            }
+    //for(int i = 0 ; i <= 5 ; i++) std::cout << wplata[i] << std::endl; //petle do debugowania
+    suma = static_cast<float>(wplata[0]*5 + wplata[1]*2 + wplata[2]*1 + wplata[3]*0.5 + wplata[4]*0.2 + wplata[5]*0.1); //rzutowanie na float zeby obsluzyc grosze
+    //for(int i = 0 ; i <= 5 ; i++) std::cout << wplata[i] << std::endl; //petle do debugowania
+    }
+}
+void Menu::wyswietl_menu_wyboru()
+{
+std::cout << "Coca Cola";
+std::cout << "\nFanta";
+std::cout << "\nPepsi";
+std::cout << "\nSprite";
+//Db::test();
+}
+
+void Menu::wyswietl_menu_serwisowe()
+{
+
+}
--- automat/menu.h
+++ automat/menu.h
@@ -1,1 +1,20 @@
 #include "naglowki.h"
+#pragma once
+class Menu :protected Transakcje
+{
+protected:
+    bool flaga_menu_wplaty; //flaga petli wyswietlania
+    bool flaga_menu_wyboru; //flaga petli wyswietlania
+    bool flaga_menu_serwisowe; //flaga petli wyswietlania
+    int wybor; //zmienna do switcha
+    float suma; //zmienna przechowujaca sume wplaty
+    int wplata[6]; //tablica przechowujaca dane o ilosci wrzuconych monet
+public:
+    void wyswietl_menu_wplaty();
+    void wyswietl_menu_wyboru();
+    void wyswietl_menu_serwisowe();
+
+    Menu();
+    ~Menu();
+};
+
