4bb9ebcb69ecb1a871aac0c96c42d3b7ee31b22c
KS <KS@KS>
2020-05-30 05:53:04 +0200

    struktura klasy
    struktura klasy, setter i getter

--- automat/transakcje.cpp
+++ automat/transakcje.cpp
@@ -1,1 +1,19 @@
 #include "transakcje.h"
+
+void Transakcje::set_data(std::vector<int> _data){ //pobiera dane z podanego wektora
+    data = _data;
+}
+
+std::vector<int> Transakcje::get_data(){ //wypisuje dane do podanego wektora
+    return data;
+}
+
+Transakcje::Transakcje()
+{
+
+}
+
+Transakcje::~Transakcje()
+{
+
+}
--- automat/transakcje.h
+++ automat/transakcje.h
@@ -1,2 +1,15 @@
 #include "naglowki.h"
 #pragma once
+
+class Transakcje
+{
+//friend class Menu
+public:
+    void set_data(std::vector<int>);
+    std::vector<int> get_data();
+    Transakcje();
+    ~Transakcje();
+
+protected:
+    std::vector<int> data;  //pola 0-5 wplacone monety, 6-11 monety do wydawania reszty, 12-17 ilosc napojow w auromacie
+};
