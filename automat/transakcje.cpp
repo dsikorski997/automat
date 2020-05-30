95c0cd9873465a6e136cac1778b205db59d97555
KS <KS@KS>
2020-05-30 06:08:02 +0200

    funkcja sumujaca
    dodanie funkcji sumujacej wektor na potrzeby innych funkcji

--- automat/transakcje.cpp
+++ automat/transakcje.cpp
@@ -12,6 +12,10 @@ Transakcje::test(){
     for(size_t i = 0 ; i < data.size() ; i++) std::cout << data[i] << std::endl;
 }
 
+double Transakcje::calc(std::vector<int> pay){
+    return static_cast<float>(pay[0]*5 + pay[1]*2 + pay[2]*1 + pay[3]*0.5 + pay[4]*0.2 + pay[5]*0.1);
+}
+
 Transakcje::Transakcje()
 {
 
--- automat/transakcje.h
+++ automat/transakcje.h
@@ -8,9 +8,11 @@ public:
     void set_data(std::vector<int>);
     std::vector<int> get_data();
     void test()
+
     Transakcje();
     ~Transakcje();
 
 protected:
     std::vector<int> data;  //pola 0-5 wplacone monety, 6-11 monety do wydawania reszty, 12-17 ilosc napojow w auromacie
+    double calc(std::vector<int>);
 };
