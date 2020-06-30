b7da6d69861d789fdc11b9a9e95af85829a39367
DS <DS@DS>
2020-06-30 19:08:13 +0200

    menu wplaty
    ogolna petla programu, funkcja menu wplaty, zmienne potrzebne do dzialania funkcji

--- automat/main.cpp
+++ automat/main.cpp
@@ -9,17 +9,60 @@
     bool flaga_menu_serwisowe = 0; //flaga petli wyswietlania
     bool flaga_menu_srv_wplac = 0; //flaga podmenu serwisowego
     bool flaga_menu_srv_dodaj = 0; //flaga podmenu serwisowego
+    int wybor = 0; //zmienna do switcha
+    float suma = 0; //zmienna przechowujaca sume wplaty
+    std::vector<int> wplata{0,0,0,0,0,0}; //tablica przechowujaca dane o ilosci wrzuconych monet
 
 
 
 int main()
 {
-Db db;
-Transakcje transakcje;
-transakcje.set_data(db.get_data());
+Db db; //tworzy obiekt bazy
+Transakcje transakcje;  //tworzy obiekt transakcji
+transakcje.set_data(db.get_data()); //przepisuje dane z bd do transakcje przez settery i gettery
 
-transakcje.test();
+Sleep(4000);
 
-db.set_data(transakcje.get_data());
+//stworzenie instancji klasy dzedziczonej tworzy tez instancje klasy bazowej dlatego to dziala
+while(menu){
+//flaga_menu_wplaty = 1;
+    while(flaga_menu_wplaty){
+        system("cls");
+        std::cout<<"Napoje w ofercie\n";
+        std::cout <<"Coca Cola \t2zl\n";
+        std::cout <<"Fanta \t\t2zl\n";
+        std::cout <<"Pepsi \t\t2zl\n";
+        std::cout <<"Sprite\t\t2zl\n";
+        std::cout <<"Burn \t\t3zl\n";
+        std::cout <<"Red Bull \t4.50zl\n";
+        std::cout<<"Automat przyjmuje monety 5, 2, 1, 0.50, 0.20, 0.10 zl\n";
+        std::cout<<"Wpisz jakie wrzuciles monety\n";
+        std::cout<<"1: 5zl\n";
+        std::cout<<"2: 2zl\n";
+        std::cout<<"3: 1zl\n";
+        std::cout<<"4: 0.50zl\n";
+        std::cout<<"5: 0.20zl\n";
+        std::cout<<"6: 0.10zl\n";
+        std::cout<<"7: Zakoncz wplacanie\n";
+        std::cout<<"8: Zwrot pieniedzy\n";
+        std::cout<<"9: Zakoncz program\n";
+        std::cout<< "suma: " << std::fixed << std::setprecision(2) << suma <<std::endl;
+        std::cin>>wybor;
+
+            switch(wybor){
+                case 1: wplata[0]++; break;
+                case 2: wplata[1]++; break;
+                case 3: wplata[2]++; break;
+                case 4: wplata[3]++; break;
+                case 5: wplata[4]++; break;
+                case 6: wplata[5]++; break;
+                case 7: flaga_menu_wyboru = 1; flaga_menu_wplaty = 0; break;
+                case 8: system("cls"); std::cout << "Zwrot " << suma << "zl"; for(size_t z = 0 ; z <= 5 ; z++)wplata[z]=0; Sleep(4000); break;
+                case 9: menu = 0; flaga_menu_wplaty = 0; break; //wyjscie z programuplan jest taki
+                case 13: sprawdz_haslo(flaga_menu_serwisowe, flaga_menu_wplaty, flaga_menu_wyboru); break;  //ukryta opcja wejscia w menu serwisowe
+            }
+    suma = static_cast<float>(wplata[0]*5 + wplata[1]*2 + wplata[2]*1 + wplata[3]*0.5 + wplata[4]*0.2 + wplata[5]*0.1); //rzutowanie na float zeby obsluzyc grosze
+    }
+}
 return 0;
 }
