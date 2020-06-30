a73985590a181bb6d62c60eff731d3bb561c0b99
DS <DS@DS>
2020-06-30 19:05:00 +0200

    zmienne menu
    zmienne obslugujace petle poszczegolnych menu

--- automat/main.cpp
+++ automat/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
 #include "naglowki.h"
 #include "db.h"
-using namespace std;
+#include "transakcje.h"
+#include "menu.h"
+
+    bool menu = 1;
+    bool flaga_menu_wplaty = 1; //flaga petli wyswietlania
+    bool flaga_menu_wyboru = 0; //flaga petli wyswietlania
+    bool flaga_menu_serwisowe = 0; //flaga petli wyswietlania
+    bool flaga_menu_srv_wplac = 0; //flaga podmenu serwisowego
+    bool flaga_menu_srv_dodaj = 0; //flaga podmenu serwisowego
+
+
 
 int main()
 {
